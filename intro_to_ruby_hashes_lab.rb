36f893b69f81cd0afbe7d3ed1f4a3efe2a8ad0d4
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-07 02:34:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,8 @@ end
 
 def update_counting_hash(hash, key)
   if update_counting_hash(hash, key)
-    hash[:key] += 1 
+    hash[key] += 1 
   else
-    hash[:key] = 1
+    hash[key] = 1
   end
 end
